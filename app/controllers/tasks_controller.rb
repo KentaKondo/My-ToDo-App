7959537fabf1e022fc46f00bae29451f6d840a48
KEN <kenkenkonkon1207@gmail.com>
2020-11-30 18:22:55 +0900

    commentモデルを作成

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,14 +3,16 @@ class TasksController < ApplicationController
   
   
   def show
-    @task = Task.find(params[:task_id])
+    @task = Task.find(params[:id])
+    @board = Board.find(params[:board_id])
   end
 
+  
   def new
     board = Board.find(params[:board_id])
     @task = board.tasks.build
   end
-
+  
   def create
     board = Board.find(params[:board_id])
     @task = board.tasks.create(task_params)
@@ -21,13 +23,33 @@ class TasksController < ApplicationController
       render:new
     end
   end
+  
+  def edit
+    @task = Task.find(params[:id])
+    @board = Board.find(params[:board_id])
+  end
 
+  def update
+    @task = Task.find(params[:id])
+    if @task.update(task_params)
+    redirect_to board_task_path(id: @task, board_id: @board), notice: '更新出来ました'
+    else
+    flash.now[:error] = '更新出来ませんでした'
+    render :edit
+    end
+  end
 
+  def destroy
+    @board = Board.find(params[:board_id])
+    @task = current_user.tasks.find(params[:id])
+    @task.destroy!
+    redirect_to board_path(id: @board), notice: '削除に成功しました'
+  end
 
 private
 
 def task_params
-  params.require(:task).permit(:title, :content, :user_id)
+  params.require(:task).permit(:title, :content, :user_id, :eyecatch)
 end
 
 end
