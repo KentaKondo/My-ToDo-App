d3fefa00c138c9df57691466c664ed73d709fca7
KEN <kenkenkonkon1207@gmail.com>
2020-12-03 13:41:09 +0900

    本番環境では.envを読み込まない様に設定

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module MyTodoApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-
+    if Rails.env.development? || Rails.env.test?
     Bundler.require(*Rails.groups)
     Dotenv::Railtie.load
     # Settings in config/environments/* take precedence over those specified here.
